9f61f4d836e18818c035b22de6f9f75dc8a957f9
Diansavov <diansavov2@abv.bg>
2025-10-22 00:11:02 +0300

    ShittyEsLintConfigs

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // Treat fatal runtime problems as errors
+      "no-undef": "error",
+      "no-redeclare": "error",
+      "no-unused-expressions": "error",
+
+      // Treat optimization / style as warnings
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "complexity": ["warn", { "max": 10 }]
     },
   },
-])
+  {
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+
+      "react/react-in-jsx-scope": "off",   // JSX React import not required
+      "react/prop-types": "off",           // Optional
+      "react/jsx-no-target-blank": "off", // Optional
+    },
+  },
+]);
